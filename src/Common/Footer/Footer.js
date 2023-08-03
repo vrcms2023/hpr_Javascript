0f855c3a697bfca21f2cf28d6a2394b4f589db14
Vara <prasadnti@gmail.com>
2023-08-03 09:00:19 +0530

    Header & Footer Changes

--- src/Common/Footer/Footer.js
+++ src/Common/Footer/Footer.js
@@ -1,52 +1,55 @@
 import React from 'react'
-import Title from '../Title'
 import { Link } from "react-router-dom";
+
+import Logo from '../../../src/Images/hpr-infra-logo.png'
 
 import './Styles.css'
 
 const Footer = () => {
   return (
     <>
-    <footer className='text-center bg-secondary text-dark py-5 footerTop'>
+    <footer className='text-center text-dark py-3 footerTop'>
       <div className='container py-3'>
         <div className='row'>
-          <div className='col-md-4 text-start'>
-              <Title title="HPR Info" cssClass="text-white fs-4"/>
-              <p className=''>
-              HPR Infra Projects has instantly made a distinctive mark on the cityscape of Hyderabad: the city in which the group is headquartered, as it understands the city with an instinctive ease sharing with it a glorious past and a promising future.
-              </p>
-            </div>
+          <div className='col-md-3 '>
+            <address className='text-center text-sm-start'>
+              <strong className='fs-4'>Address</strong><br />
+              101, Silicon Towers, <br />
+              Image Garden Road, <br />
+              Madhapur, <br />
+              Hyderabad - 500081.
+            </address>
+          </div>
+          <hr className='d-block d-sm-none' />
+            <div className='col-md-3 text-center text-sm-start'>
+              <address>
+                <strong className='fs-4'>Phone number</strong><br />
+                <abbr title="Phone">P:</abbr> 40-40036841
+              </address>
 
-            <div className='col-md-4 text-start'>
-              <Title title="Contact Info" cssClass="text-white fs-4"/>
-              <p className='fs-5'>
-              HPR Infra Projects, <br />Flat No.404, SIRI Enclave, Srinagar Colony, <br />Main Road, Hyderabad- 500 073.
-              </p>
-              
-              <div className='d-flex justify-content-start align-items-center'>
-                <i className="fa fa-mobile fs-1 me-3" aria-hidden="true"></i>
-                <p className='fs-5 text-dark'>040 – 65224777, <br />040 - 23740171</p>
-              </div>
-              <div className='d-flex justify-content-start align-items-center'>
-                <i className="fa fa-envelope-o fs-5 fw-bold me-3" aria-hidden="true"></i>
-                <a href='mailto:contact@hprinfraprojects.com' className='fs-5 text-dark'> contact@hprinfraprojects.com</a>
-              </div>
+              <address>
+                <strong className='fs-4'>Email</strong><br />
+                <abbr title="Phone">E:</abbr> <a href='mailto:contact@hprinfra.com.com' className='text-dark'> contact@contact@hprinfra.com</a>
+              </address>
             </div>
+            <hr className='d-block d-sm-none' />
+            <div className='col-md-3 text-center text-sm-start'>
+            <address>
+              <strong className='fs-4'>Social Media</strong><br />
+              <Link to="https://www.facebook.com/HPRInfraProjects" target='_blank' className='ms-0 text-underline'>Facebook</Link > <br />
+              <Link to="https://plus.google.com/+HprinfraprojectsAdibatlaHyderabad/posts" target='_blank' className='ms-0'>Google Plus</Link >
+            </address>
+          </div>
 
-            <div className='col-md-4 text-start'>
-            <Title title="Social Media" cssClass="text-white pb-3 fs-4 text-end"/>
-            <div className='d-flex justify-content-between align-items-end flex-column'>
-              <a href="#" className='fs-4 text-dark'>FACEBOOK <i className="fa fa-facebook-square fs-1 ms-2" aria-hidden="true"></i></a>
-              <a href="#" className='fs-4 text-dark'>GOOGLE PLUS<i className="fa fa-google-plus-square fs-1 ms-2" aria-hidden="true"></i></a>
-              <a href="#" className='fs-4 text-dark'>LINKED IN<i className="fa fa-linkedin-square fs-1 ms-2" aria-hidden="true"></i></a>
-              </div>
-            </div>
+          <div className='col-md-3 text-start d-none d-md-block'>
+              <img src={Logo} alt=""/>
+          </div>
       </div>
       </div>
     </footer>
     <footer className='text-center text-white py-4 footerLinks'>
-      <div className='container d-flex justify-content-between align-items-center flex-row flex-md-column flex-sm-column'>
-        <ul className='d-flex justify-content-between align-items-center list-unstyled m-0'>
+      <div className='container d-flex justify-content-between align-items-center flex-column flex-sm-row  '>
+        <ul className='d-lg-flex flex-md-wrapjustify-content-between align-items-center list-unstyled m-0'>
             <li >
               <Link to="/" className='ms-0'>Home</Link >
             </li>
@@ -66,7 +69,7 @@ const Footer = () => {
               <Link  to="/contact" >Contact Us</Link >
             </li>
         </ul>
-        <ul className='d-flex justify-content-between align-items-center list-unstyled m-0'>
+        <ul className='d-lg-flex justify-content-between align-items-center list-unstyled m-0'>
           <li >
             <Link to="/">Privacy Policy</Link >
           </li>
