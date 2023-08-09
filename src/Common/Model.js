698137e14f0805b4d848140b8164d57e344e56cf
Vara <prasadnti@gmail.com>
2023-08-09 12:08:57 +0530

    footer, model, about, new&updates and css file changes

--- src/Common/Model.js
+++ src/Common/Model.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Model = ({obj, closeModel} ) => {
-    const {title, description} = obj
+    const {title, description, dec1, dec2, dec3, cr, crm} = obj
   return (
     <div className="modal d-block modal-lg" tabIndex="-1">
         <div className="modal-dialog modal-dialog-centered">
@@ -11,7 +11,12 @@ const Model = ({obj, closeModel} ) => {
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={closeModel}></button>
             </div>
             <div className="modal-body">
-                <p className='px-4 py-3'>{description}</p>
+                {description && <p className='px-4 py-2'>{description}</p>}
+                {dec1 &&  <p className='px-4 pb-1'>{dec1}</p>}
+                {dec2 &&  <p className='px-4 pb-1'>{dec2}</p>}
+                {dec3 &&  <p className='px-4 pb-1'>{dec3}</p>}
+                {cr &&  <p className='px-4 pb-1'>{cr}</p>}
+                {crm &&  <p className='px-4 pb-1'>{crm}</p>}
             </div>
             {/* <div className="modal-footer text-center">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={closeModel}>Close</button>
