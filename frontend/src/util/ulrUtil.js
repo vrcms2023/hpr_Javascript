dbadb7a789c2100b9b39250cef7eab283a31e56a
Ramakrishna <Rama@jetti@gmail.com>
2023-08-31 13:28:02 +0530

    updated changes

--- frontend/src/util/ulrUtil.js
+++ frontend/src/util/ulrUtil.js
@@ -1,8 +1,7 @@
 export function getBaseURL() {
-  // return process.env.NODE_ENV !== "production"
-  //   ? "http://127.0.0.1:5001"
-  //   : import.meta.env.VITE_SERVER_URL;
-  return "http://127.0.0.1:5001"
+  return process.env.NODE_ENV !== "production"
+    ? "http://127.0.0.1:5001"
+    : import.meta.env.VITE_SERVER_URL;
 }
 
 export function removeActiveClass() {
