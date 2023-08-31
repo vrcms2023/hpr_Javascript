fd57d251a72b1ce326db1a8d1922eaa8767a5aa8
Vara <prasadnti@gmail.com>
2023-08-31 10:59:34 +0530

    Responsive issues Admin Main page

--- frontend/src/Admin/Pages/Login/MainPage.jsx
+++ frontend/src/Admin/Pages/Login/MainPage.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import Title from "../../../Common/Title";
 import { getCookie } from "../../../util/cookieUtil";
 
+import './MainPage.css';
+
 const MainPage = () => {
   const [isSuperAdmin, setisSuperAdmin] = useState("");
   useEffect(() => {
@@ -10,43 +12,42 @@ const MainPage = () => {
   }, []);
   return (
     <div
-      className="d-flex flex-column justify-content-center align-items-center"
-      style={{ marginTop: "200px" }}
+      className="d-flex flex-column justify-content-center align-items-center adminMain"
     >
       <Title
         title="Page Content's Customization"
-        cssClass="text-dark fs-5 mb-4 fw-bold"
+        cssClass="text-dark fs-3 mb-4 fw-bold text-center"
       />
-      <ul className="list-group w-25">
-        <li className="list-group-item list-group-item-action text-center py-4">
-          <Link to="/dashboard" className="blue-500 text-decoration-none fs-5">
+      <ul className="list-group mainLinks">
+        <li className="list-group-item list-group-item-action text-center border-secondary py-4">
+          <Link to="/dashboard" className="blue-500 text-decoration-none fs-3">
             Dashboard
           </Link>
         </li>
-        <li className="list-group-item list-group-item-action text-center py-4">
-          <Link to="/addproject" className="text-decoration-none blue-500 fs-5">
+        <li className="list-group-item list-group-item-action text-center border-secondary py-4">
+          <Link to="/addproject" className="text-decoration-none blue-500 fs-3">
             Add Project
           </Link>
         </li>
-        <li className="list-group-item list-group-item-action text-center py-4">
-          <Link to="/adminNews" className="blue-500 text-decoration-none fs-5">
+        <li className="list-group-item list-group-item-action text-center border-secondary py-4">
+          <Link to="/adminNews" className="blue-500 text-decoration-none fs-3">
             New & Updates
           </Link>
         </li>
-        <li className="list-group-item list-group-item-action text-center py-4">
+        <li className="list-group-item list-group-item-action text-center border-secondary py-4">
           <Link
             to="/testimonial"
-            className="blue-500 text-decoration-none fs-5"
+            className="blue-500 text-decoration-none fs-3"
           >
             Testimonials
           </Link>
         </li>
 
         {isSuperAdmin ? (
-          <li className="list-group-item list-group-item-action text-center py-4">
+          <li className="list-group-item list-group-item-action text-center border-secondary py-4">
             <Link
               to="/userAdmin"
-              className="blue-500 text-decoration-none fs-5"
+              className="blue-500 text-decoration-none fs-3"
             >
               User Administration
             </Link>
