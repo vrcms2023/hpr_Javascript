b06a54feff5c9d67c98b79c7b5b6300ad61d197c
Ramakrishna <Rama@jetti@gmail.com>
2023-08-31 18:43:45 +0530

    updated server changes

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")),
   );
 }
-
+app.use(express.static(path.join(__dirname,"/frontend/public")))
 // Middleware
 app.use(notFound);
 app.use(errorHandler);
